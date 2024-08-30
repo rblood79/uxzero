ed7a57cac2bedfe56bcc371160340072034f9efd
rblood79 <rblood79@gmail.com>
2024-08-30 09:45:02 +0900

    f

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'screens/login_screen.dart';
+//import 'screens/login_screen.dart';
 import 'screens/home_screen.dart';
 import 'firebase_options.dart'; // firebase_options.dart를 import
 
