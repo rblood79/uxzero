048bef15df794abf96adf5da603c551f4e40319a
rblood79 <rblood79@gmail.com>
2024-09-19 00:11:15 +0900

    resi

--- lib/ui/work_area copy.dart
+++ lib/ui/work_area copy.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:remixicon/remixicon.dart';
 import '../models/selected_widget_model.dart';
 import '../widgets/container_widget.dart';
 import '../widgets/text_widget.dart';
@@ -306,16 +307,20 @@ class _WorkAreaState extends State<WorkArea> {
               decoration: childProperties.decoration, // 수정: BoxDecoration 사용
               child: childProperties.type == WidgetType.text
                   ? Align(
-                      alignment: childProperties.alignment ?? Alignment.center, // 기본 정렬 설정
+                      alignment: childProperties.alignment ??
+                          Alignment.center, // 기본 정렬 설정
                       child: Text(
                         childProperties.label,
-                        textAlign: childProperties.textAlign ?? TextAlign.center, // 텍스트 정렬
-                        style: TextStyle(fontSize: childProperties.fontSize ?? 12.0,
+                        textAlign: childProperties.textAlign ??
+                            TextAlign.center, // 텍스트 정렬
+                        style: TextStyle(
+                          fontSize: childProperties.fontSize ?? 12.0,
                           color: Colors.black,
                         ),
                       ),
                     )
-                  : _buildDragTargetForContainer(childProperties, selectedWidgetModel),
+                  : _buildDragTargetForContainer(
+                      childProperties, selectedWidgetModel),
             );
           },
         ),
@@ -375,125 +380,231 @@ class _WorkAreaState extends State<WorkArea> {
     }
   }
 ///////////////////////////////////////////////////
-OverlayEntry _buildMultipleOverlay(List<_OverlayInfo> overlayInfoList, SelectedWidgetModel selectedWidgetModel) {
+
+  OverlayEntry _buildMultipleOverlay(List<_OverlayInfo> overlayInfoList,
+    SelectedWidgetModel selectedWidgetModel) {
   final selectedOverlayInfo = overlayInfoList.removeAt(0);
   overlayInfoList.add(selectedOverlayInfo);
 
+  // 선택된 위젯 중 리사이즈 포인트가 나타나야 할 위젯을 찾는 함수
+  List<WidgetProperties> _findTopMostParents(
+      List<WidgetProperties> selectedWidgets) {
+    // 최상위 부모나 동일 레벨 위젯 모두 포함하는 리스트 반환
+    List<WidgetProperties> topMostParents = [];
+    for (var widget in selectedWidgets) {
+      var currentWidget = widget;
+      // 부모가 선택된 위젯 중 없거나 최상위 부모를 찾는다
+      while (currentWidget.parent != null &&
+          selectedWidgets.contains(currentWidget.parent)) {
+        currentWidget = currentWidget.parent!;
+      }
+      if (!topMostParents.contains(currentWidget)) {
+        topMostParents.add(currentWidget);
+      }
+    }
+    return topMostParents;
+  }
+
+  // 선택된 위젯들 중 리사이즈 포인트가 나타나야 할 위젯들 찾기
+  final topMostParents =
+      _findTopMostParents(selectedWidgetModel.selectedWidgetProperties);
+
   return OverlayEntry(
-    builder: (context) => Stack(
-      children: overlayInfoList.asMap().entries.map((entry) {
-        final index = entry.key;
-        final overlayInfo = entry.value;
-        final colors = [
-          Colors.red,
-          Colors.orange,
-          Colors.green,
-          Colors.lightBlue,
-          Colors.lightBlueAccent,
-          Colors.blue,
-          Colors.blueAccent,
-          Colors.purple,
-        ];
-        final color = colors[index % colors.length];
+    builder: (context) => LayoutBuilder(
+      builder: (context, constraints) {
+        final maxParentWidth = constraints.maxWidth; // 부모의 최대 너비
+        final maxParentHeight = constraints.maxHeight; // 부모의 최대 높이
 
-        // 크기 조정을 위한 변수
-        double overlayWidth = overlayInfo.size.width;
-        double overlayHeight = overlayInfo.size.height;
+        return Stack(
+          children: overlayInfoList.asMap().entries.map((entry) {
+            final index = entry.key;
+            final overlayInfo = entry.value;
+            final colors = [
+              Colors.red,
+              Colors.orange,
+              Colors.green,
+              Colors.lightBlue,
+              Colors.lightBlueAccent,
+              Colors.blue,
+              Colors.blueAccent,
+              Colors.purple,
+            ];
+            final color = colors[index % colors.length];
 
-        return StatefulBuilder(
-          builder: (context, setState) {
-            return Stack(
-              children: [
-                Positioned(
-                  left: overlayInfo.offset.dx,
-                  top: overlayInfo.offset.dy,
-                  child: IgnorePointer(
-                    ignoring: true,
-                    child: Container(
-                      width: overlayWidth.roundToDouble(),  // 최종 그릴 때는 반올림
-                      height: overlayHeight.roundToDouble(), // 최종 그릴 때는 반올림
-                      decoration: BoxDecoration(
-                        border: Border.all(
+            // 선택된 위젯인지 확인
+            final isSelected = selectedWidgetModel.selectedWidgetProperties
+                .contains(overlayInfo.properties);
+            // 리사이즈 포인트가 나타나야 할 위젯인지 확인
+            final isTopMostParent =
+                topMostParents.contains(overlayInfo.properties);
+
+            // 크기 조정을 위한 변수
+            double overlayWidth = overlayInfo.size.width;
+            double overlayHeight = overlayInfo.size.height;
+
+            return StatefulBuilder(
+              builder: (context, setState) {
+                return Stack(
+                  children: [
+                    // 가이드라인 사각형
+                    Positioned(
+                      left: overlayInfo.offset.dx,
+                      top: overlayInfo.offset.dy,
+                      child: IgnorePointer(
+                        ignoring: true,
+                        child: Container(
+                          width: overlayWidth.roundToDouble(), // 최종 그릴 때는 반올림
+                          height: overlayHeight.roundToDouble(), // 최종 그릴 때는 반올림
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: color,
+                              width: 1.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    // label
+                    Positioned(
+                      left: overlayInfo.offset.dx,
+                      top: overlayInfo.offset.dy - 30,
+                      child: GestureDetector(
+                        onTap: () {
+                          selectedWidgetModel.clearSelection();
+                          selectedWidgetModel
+                              .selectWidget(overlayInfo.properties);
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 16.0, vertical: 8.0),
+                          decoration: BoxDecoration(
+                            color: color.withOpacity(1.0),
+                            borderRadius: BorderRadius.circular(0.0),
+                          ),
+                          child: Text(
+                            overlayInfo.properties.label,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                              fontWeight: FontWeight.normal,
+                              decoration: TextDecoration.none,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    // Width 표시 (가로 하단 가이드라인 안쪽에 위치)
+                    Positioned(
+                      left: overlayInfo.offset.dx, // 중앙에 맞춤
+                      top: overlayInfo.offset.dy + overlayHeight, // 하단 가이드라인 안쪽에 위치
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 16.0, vertical: 8.0),
+                        color: color,
+                        child: Text(
+                          '${overlayWidth.round()} px', // 가로 크기 표시
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 12,
+                            fontWeight: FontWeight.normal,
+                            decoration: TextDecoration.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                    // Height 표시 (세로 우측 가이드라인 안쪽에 위치, -90도 회전)
+                    Positioned(
+                      left: overlayInfo.offset.dx +
+                          overlayWidth -
+                          19, // 우측 가이드라인 안쪽에 위치
+                      top: overlayInfo.offset.dy + 19, // 세로 가운데에 맞춤
+                      child: Transform.rotate(
+                        angle: -90 * 3.1415927 / 180, // -90도 회전
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 16.0, vertical: 8.0),
                           color: color,
-                          width: 2.0,
+                          child: Text(
+                            '${overlayHeight.round()} px', // 세로 크기 표시
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                              fontWeight: FontWeight.normal,
+                              decoration: TextDecoration.none,
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ),
-                Positioned(
-                  left: overlayInfo.offset.dx,
-                  top: overlayInfo.offset.dy - 30,
-                  child: GestureDetector(
-                    onTap: () {
-                      selectedWidgetModel.clearSelection();
-                      selectedWidgetModel.selectWidget(overlayInfo.properties);
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                      decoration: BoxDecoration(
-                        color: color.withOpacity(1.0),
-                        borderRadius: BorderRadius.circular(0.0),
-                      ),
-                      child: Text(
-                        overlayInfo.properties.label,
-                        style: const TextStyle(
-                          color: Colors.black,
-                          fontSize: 14,
-                          fontWeight: FontWeight.normal,
-                          decoration: TextDecoration.none,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                // 우측 하단에 리사이즈 포인트 추가
-                Positioned(
-                  left: overlayInfo.offset.dx + overlayWidth - 24, // 우측 하단 위치
-                  top: overlayInfo.offset.dy + overlayHeight - 24, // 우측 하단 위치
-                  child: GestureDetector(
-                    onPanUpdate: (details) {
-                      setState(() {
-                        // 오버레이 크기만 변경, 소수점 유지
-                        overlayWidth += details.delta.dx;
-                        overlayHeight += details.delta.dy;
+                    // 선택된 위젯 중 리사이즈 포인트 표시
+                    if (isTopMostParent)
+                      Positioned(
+                        left: overlayInfo.offset.dx +
+                            overlayWidth - 1, // 우측 하단 위치
+                        top: overlayInfo.offset.dy +
+                            overlayHeight - 1, // 우측 하단 위치
+                        child: GestureDetector(
+                          onPanUpdate: (details) {
+                            setState(() {
+                              // 오버레이 크기만 변경, 소수점 유지
+                              overlayWidth += details.delta.dx;
+                              overlayHeight += details.delta.dy;
 
-                        // 최소 크기 제한 (소수점은 유지)
-                        if (overlayWidth < 48) overlayWidth = 48;
-                        if (overlayHeight < 48) overlayHeight = 48;
-                      });
-                    },
-                    onPanEnd: (details) {
-                      // 리사이즈가 끝난 후 실제 위젯의 크기를 업데이트, 소수점 제거 후 반올림 적용
-                      setState(() {
-                        overlayInfo.properties.width = overlayWidth.roundToDouble();
-                        overlayInfo.properties.height = overlayHeight.roundToDouble();
-                        selectedWidgetModel.notifyListeners(); // 모델에 변경 사항 알림
-                      });
-                    },
-                    child: Container(
-                      width: 24,
-                      height: 24,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        border: Border.all(
-                          color: color,
-                          width: 2.0,
+                              // 최소 크기 제한 (소수점은 유지)
+                              if (overlayWidth < 48) overlayWidth = 48;
+                              if (overlayHeight < 48) overlayHeight = 48;
+
+                              // 부모의 크기를 초과하지 않도록 제한
+                              if (overlayInfo.offset.dx + overlayWidth >
+                                  maxParentWidth) {
+                                overlayWidth =
+                                    maxParentWidth - overlayInfo.offset.dx;
+                              }
+                              if (overlayInfo.offset.dy + overlayHeight >
+                                  maxParentHeight) {
+                                overlayHeight =
+                                    maxParentHeight - overlayInfo.offset.dy;
+                              }
+                            });
+                          },
+                          onPanEnd: (details) {
+                            // 리사이즈가 끝난 후 실제 위젯의 크기를 업데이트, 소수점 제거 후 반올림 적용
+                            setState(() {
+                              overlayInfo.properties.width =
+                                  overlayWidth.roundToDouble();
+                              overlayInfo.properties.height =
+                                  overlayHeight.roundToDouble();
+                              selectedWidgetModel.notifyListeners(); // 모델에 변경 사항 알림
+                            });
+                          },
+                          child: Container(
+                            width: 30,
+                            height: 30,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: color,
+                                width: 0.0,
+                              ),
+                              color: color.withOpacity(0.3),
+                            ),
+                            child: Transform.rotate(
+                              angle: -45 * 3.1415927 / 180, // -90도 회전
+                              child: const Icon(
+                                Remix.expand_up_down_line, // 리사이즈 아이콘
+                                size: 24,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
                         ),
                       ),
-                      child: const Icon(
-                        Icons.crop_square, // 리사이즈 아이콘
-                        size: 16,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+                  ],
+                );
+              },
             );
-          },
+          }).toList(),
         );
-      }).toList(),
+      },
     ),
   );
 }
