2e7091be62fc9d9dc894de14c6278efe0a156715
rblood79 <rblood79@users.noreply.github.com>
2024-09-24 16:00:05 +0000

    deploy: 43982c8f6f9077499d978bf3be93ed78979ee72a

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"c9b9d5780da342eb3f0f5e439a7db06f7d1125
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4224745868"
+    serviceWorkerVersion: "1438536406"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.json": "37d1d930e5149d791987c3d184e20db
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
-"flutter_bootstrap.js": "627dd6e1c27d31eddeea4dbf7ac2d6eb",
+"flutter_bootstrap.js": "0c45d06b86a08d2e315e4345c4d10909",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "index.html": "67f1ce01d32dd8578fa3ca64155c565d",
