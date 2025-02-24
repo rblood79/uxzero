6e943a2a31d585d65afe37d9aaabfdb602beee03
rblood79 <rblood79@users.noreply.github.com>
2025-02-24 01:44:11 +0000

    deploy: abe79166a1e795bd155c203fc5d33b7570f746ef

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,7 +7,7 @@ const RESOURCES = {"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb0489
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"flutter_bootstrap.js": "5c2272f844644216dd2f2d809bc19342",
+"flutter_bootstrap.js": "4f094a0bdcb66358cdf2f449b08576fb",
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "canvaskit/skwasm.wasm": "9f0c0c02b82a910d12ce0543ec130e60",
 "canvaskit/chromium/canvaskit.wasm": "b1ac05b29c127d86df4bcfbf50dd902a",
